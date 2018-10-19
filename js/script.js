05ac363dc64bcf9e4b1426141e73085cd530f842
glen <glen_sale@yahoo.com>
2018-10-19 12:12:15 -0700

    it will try to make an alert using custom script but its not working right now

--- js/script.js
+++ js/script.js
@@ -4,10 +4,20 @@ event.preventDefault()
   var name = $("input#nameInput").val();
   var email = $("input#emailInput").val();
   var address = $("input#addressInput").val();
+  var interest = $("input#interestInput").val();
 
-    $(".firstname").text(firstnameInput);
-    $(".lastname").text(lastnameInput);
-    $(".address").text(addressInput);
+    $("name").text(firstnameInput);
+    $("email").text(emailInput);
+    $("Adress").text(addressInput);
+      $("interest").text(interestInput);
 
-  });
-});
+      if(name === email && address === intrest) {
+     $("#c").show();
+     $("Java").hide();
+     $("PHP").hide();
+     $("Ruby").hide();
+     S("CSS").hide();
+
+   }
+ });
+ });
