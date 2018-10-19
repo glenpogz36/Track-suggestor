a52bdb9c15d7ae592a0cf333185f39ad79009a2a
glen <glen_sale@yahoo.com>
2018-10-19 14:32:38 -0700

    it will try another method of else if(fail)

--- js/script.js
+++ js/script.js
@@ -1,16 +1,43 @@
 $(document).ready(function() {
 $("#infos").submit(function(event)  {
 event.preventDefault()
-    var c = parseInt($("input#c").val(1));
-    var java = parseInt($("input#javat").val(2));
-    var php = parseInt($("input#php").val(3));
-    var ruby = parseInt($("input#ruby").val(4));
-    var css = parseInt($("input#css").val(5));
-    if(c || !java || !java || !php || !ruby || !css)
-    $("#c").show(1);
-     $("#java").hide(2);
-     $("#php").hide(3);
-     $("#ruby").hide(4);
-     $("#css").hide(5);
-   })
+    var c = parseInt($("input#c").val());
+    var java = parseInt($("Java").val());
+    var php = parseInt($("input#PHP").val());
+    var ruby = parseInt($("input#Ruby").val());
+    var css = parseInt($("input#CSS").val());
+
+    if(c) {
+      $("#c").show();
+      $("#Java").hide();
+      $("#PHP").hide();
+      $("#RUBY").hide();
+      $("#CSS").hide();
+    } else if (java) {
+      $("#c").hide();
+      $("#Java").show();
+      $("#PHP").hide();
+      $("#RUBY").hide();
+      $("#CSS").hide();
+    } else if (php) {
+      $("#c").hide();
+      $("#Java").hide();
+      $("#PHP").show();
+      $("#RUBY").hide();
+      $("#CSS").hide();
+    }  else if (ruby) {
+       $("#c").hide();
+       $("#Java").hide();
+       $("#PHP").hide();
+       $("#RUBY").show();
+       $("#CSS").hide();
+    } else {
+      $("#c").hide();
+      $("#Java").hide();
+      $("#PHP").hide();
+      $("#RUBY").hide();
+      $("#CSS").show();
+    }
+    
  });
+  });
