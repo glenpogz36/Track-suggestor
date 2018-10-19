08fd32bb802203bbc6e2bea0334d15f24e84da73
glen <glen_sale@yahoo.com>
2018-10-19 13:53:58 -0700

    added logos for programming languages

--- js/script.js
+++ js/script.js
@@ -7,10 +7,10 @@ event.preventDefault()
     var ruby = parseInt($("input#ruby").val(4));
     var css = parseInt($("input#css").val(5));
     if(c || !java || !java || !php || !ruby || !css)
-    $("#c").hide();
-     $("#java").show();
-     $("#php").hide();
-     $("#ruby").hide();
-     $("#css").hide();
+    $("#c").show(1);
+     $("#java").hide(2);
+     $("#php").hide(3);
+     $("#ruby").hide(4);
+     $("#css").hide(5);
    })
  });
