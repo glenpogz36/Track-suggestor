25b3fe30ff9c23d3a7dac59f57fce5946d60a5f3
glen <glen_sale@yahoo.com>
2018-10-20 19:53:20 -0700

    it will start again from the basic form (working scripts with html)

--- js/script.js
+++ js/script.js
@@ -1,43 +1,9 @@
 $(document).ready(function() {
-$("#infos").submit(function(event)  {
-event.preventDefault()
-    var c = parseInt($("input#c").val());
-    var java = parseInt($("Java").val());
-    var php = parseInt($("input#PHP").val());
-    var ruby = parseInt($("input#Ruby").val());
-    var css = parseInt($("input#CSS").val());
+  $("#formOne").submit(function(event) {
+    var person1Input = $("input#name").val();
+    $(".name").text(person1Input);
+    $("#letter").show();
 
-    if(c) {
-      $("#c").show();
-      $("#Java").hide();
-      $("#PHP").hide();
-      $("#RUBY").hide();
-      $("#CSS").hide();
-    } else if (java) {
-      $("#c").hide();
-      $("#Java").show();
-      $("#PHP").hide();
-      $("#RUBY").hide();
-      $("#CSS").hide();
-    } else if (php) {
-      $("#c").hide();
-      $("#Java").hide();
-      $("#PHP").show();
-      $("#RUBY").hide();
-      $("#CSS").hide();
-    }  else if (ruby) {
-       $("#c").hide();
-       $("#Java").hide();
-       $("#PHP").hide();
-       $("#RUBY").show();
-       $("#CSS").hide();
-    } else {
-      $("#c").hide();
-      $("#Java").hide();
-      $("#PHP").hide();
-      $("#RUBY").hide();
-      $("#CSS").show();
-    }
-    
- });
+    event.preventDefault();
   });
+});
