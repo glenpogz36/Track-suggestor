dc3537aef5c8d6cc4350fa80312ed0a0c19cfcbe
glen <glen_sale@yahoo.com>
2018-10-21 18:00:31 -0700

    it will add a decent radio button that alerts text in html

--- js/script.js
+++ js/script.js
@@ -10,8 +10,39 @@ $(document).ready(function() {
 });
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-  var address = $("input#address").val();
+    var address = $("input#address").val();
     $(".address").text(address);
         $("#letter2").show();
       });
     });
+
+    $(function(){
+      $("#formOne").submit(function(event) {
+        event.preventDefault();
+        var tracker = $("input:radio[name=tracker]:checked").val();
+
+
+    if (tracker === "C") {
+       $("#C").show();
+      $("#CSS").show();
+       $("#Java").hide();
+       $("#PHP").hide();
+       $("#Ruby").hide();
+
+    } else if (tracker === "Java") {
+      $("#C").hide();
+      $("#Java").show();
+      $("#PHP").hide();
+      $("#Ruby").hide();
+      $("#CSS").show();
+    } else if (tracker === "PHP") {
+      $("#C").hide();
+      $("#Java").hide();
+      $("#PHP").show();
+      $("#Ruby").show();
+      $("#CSS").show();
+
+
+    }
+  });
+});
